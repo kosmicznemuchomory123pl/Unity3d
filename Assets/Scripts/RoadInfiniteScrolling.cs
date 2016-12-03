8f3da9de9ed84f97d1506313a346a58fd8ca7f5a
kosmicznemuchomory123pl <karol9129@gmail.com>
2016-12-03 17:38:15 +0100

    Przewijanie drogi + oswietlenie

---
+++ Assets/Scripts/RoadInfiniteScrolling.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoadInfiniteScrolling : MonoBehaviour
+{
+    //skrypt jest przypisany do Drogi
+    //http://stackoverflow.com/questions/36947732/unity-texture-offset-not-working
+    //predkosc przewijania drogi
+    public float scrollSpeed;
+    //offset ktory nalozylismy na teksture objektu 3 wymiarowego
+    private Vector2 offset;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        offset = new Vector2(0, Time.time * scrollSpeed);
+        //ta funkcja pobierze komponent z objektu do ktorego skrypt jest przypisany
+        GetComponent<Renderer>().material.mainTextureOffset = offset;
+	}
+}
